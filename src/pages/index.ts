303fe05d8e90cbdf5e68baf77559f5aa77d3418d
Levan Ilashvili <levan@ilashvili.me>
2024-11-11 23:39:10 +0400

    just realized we dont actually need lending and borrowing lol

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,13 +1,9 @@
 import LandingPage from "./LandingPage";
 import MintRedeem from "./MintRedeem";
 import Lend from "./Lend";
-import Auction from "./Auction";
-import Borrow from "./Borrow";
 
 export {
     LandingPage,
     MintRedeem,
     Lend,
-    Auction,
-    Borrow
 }
